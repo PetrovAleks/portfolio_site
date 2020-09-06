4088f03d915e19acf15a9eec10e8476bed233c04
Petrov Aleksandr <rpm10b@gmail.com>
2020-09-06 13:49:01 +0300

    Updates

---
+++ precache-manifest.cf7b3560e0ce0341d43994074131a6a0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6709a0bb1ff1dc30512ec0e34ff6a642",
+    "url": "/portfolio_site/index.html"
+  },
+  {
+    "revision": "d5f089f9a1ff3e9e8ea9",
+    "url": "/portfolio_site/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "18b7401cb2970a80caa0",
+    "url": "/portfolio_site/static/css/main.e19e00af.chunk.css"
+  },
+  {
+    "revision": "d5f089f9a1ff3e9e8ea9",
+    "url": "/portfolio_site/static/js/2.7a76f311.chunk.js"
+  },
+  {
+    "revision": "7918f9b893310890f0a444ed75c67550",
+    "url": "/portfolio_site/static/js/2.7a76f311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b7401cb2970a80caa0",
+    "url": "/portfolio_site/static/js/main.14bc32d3.chunk.js"
+  },
+  {
+    "revision": "38b80cdc6f27077f1d19",
+    "url": "/portfolio_site/static/js/runtime-main.8852cfdd.js"
+  },
+  {
+    "revision": "7f1d312d13637c821d82452a3453f3d7",
+    "url": "/portfolio_site/static/media/GO_iteens.7f1d312d.png"
+  },
+  {
+    "revision": "e6b9aefeaa54290b6a9b6d97dfd68219",
+    "url": "/portfolio_site/static/media/Vanila-slider.e6b9aefe.png"
+  },
+  {
+    "revision": "b39a76a4744dcc5475bced54f70903a9",
+    "url": "/portfolio_site/static/media/blog.b39a76a4.png"
+  },
+  {
+    "revision": "c4155b4a5e4c2ce533abc68190fab23d",
+    "url": "/portfolio_site/static/media/galler.c4155b4a.png"
+  },
+  {
+    "revision": "20d838ab2f2ad2072e74691980f09638",
+    "url": "/portfolio_site/static/media/localStor.20d838ab.png"
+  },
+  {
+    "revision": "2641aa0ba224c5e696682fc7ff208432",
+    "url": "/portfolio_site/static/media/search_img.2641aa0b.png"
+  },
+  {
+    "revision": "c881731f56815584238264c11326b399",
+    "url": "/portfolio_site/static/media/social.c881731f.svg"
+  },
+  {
+    "revision": "6f8065126de205274b065850f6381ff9",
+    "url": "/portfolio_site/static/media/symbol-defs.6f806512.svg"
+  },
+  {
+    "revision": "924159131213931e2fb7fdd36ee1b0b2",
+    "url": "/portfolio_site/static/media/timer.92415913.png"
+  }
+]);
